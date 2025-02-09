ba459493138dd014498fc34a0fb870c16983cc1f
(KimJunGi. wnsrlrla0) <wnsrlrla0@gmail.com>
2025-02-09 21:07:49 +0900

    GameModeClass 생성.
    언리얼 7번 과제 진행 중...

---
+++ Source/PawnClass/Public/JGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "JGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PAWNCLASS_API AJGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
